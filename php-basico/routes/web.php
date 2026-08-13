10cfe013eb3cd0624766188f2c8fb55414d71f76
Lucas Ardila <lukgamer0609@gmail.com>
2026-08-13 12:08:11 -0500

    Fundamentos PHP

--- php-basico/routes/web.php
+++ php-basico/routes/web.php
@@ -6,6 +6,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hola', function () {
-    return 'Hola mundo';
+Route::get('/mi-path', function () {
+
+    $name = "Lucas";
+    $apellido = "Ardila";
+    $edad = 20;
+
+    echo "<h1>Hola $name $apellido</h1>";
+    echo "<p>Tengo $edad años</p>";
+
+    $array = ["Esto", "Es", "Un", "Array"];
+
+    foreach ($array as $item) {
+        echo "$item <br>";
+    }
+
+    return '=====================';
 });
+
+function validateEdad($edad) {
+    if ($edad >= 18 ) {
+        return "Soy mayor de edad";
+    } else {
+        return "Soy menor de edad";
+    }
+}
